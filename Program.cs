644d8232983420ef0a017af3595b6f8e9d25808f
RangellMonteiro <rangell_monteiro@hotmail.com>
2024-10-30 03:50:49 -0300

    Post para criar veículo

--- Program.cs
+++ Program.cs
@@ -5,10 +5,12 @@ using minimal_api2.Dominio.Servicos;
 using MinimalApi.DTOs;
 using MinimalApi.Infraestrutura.Db;
 using minimal_api2.Dominio.ModelViews;
-
+using MinimalApi.Dominio.Entidades;
+#region Builder
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
+builder.Services.AddScoped<IVeiculoServico, VeiculoServico>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,12 +23,15 @@ builder.Services.AddDbContext<DbContexto>(options =>
 });
 
 var app = builder.Build();
+#endregion
 
-
+#region  Home
 
 app.MapGet("/", () => Results.Json(new Home()));
+#endregion
 
-app.MapPost("/login", ([FromBody]LoginDTO loginDTO, IAdministradorServico administradorServico) =>
+#region Administradores
+app.MapPost("/administradores/login", ([FromBody]LoginDTO loginDTO, IAdministradorServico administradorServico) =>
 {
     if (administradorServico.Login(loginDTO)!= null)
         return Results.Ok("Login com sucesso");
@@ -34,14 +39,31 @@ app.MapPost("/login", ([FromBody]LoginDTO loginDTO, IAdministradorServico admini
         return Results.Unauthorized();
 });
 
+
 // Endpoint para obter todos os administradores
 app.MapGet("/administradores", async (DbContexto db) =>
 {
     var administradores = await db.Administradores.ToListAsync();
     return Results.Ok(administradores);
 });
+#endregion
 
+#region Veiculos
+app.MapPost("/veiculos", ([FromBody]VeiculoDTO veiculoDTO, IVeiculoServico veiculoServico) =>
+{
+    var veiculo = new Veiculo{
+        Nome = veiculoDTO.Nome,
+        Marca = veiculoDTO.Marca,
+        Ano = veiculoDTO.Ano,
+    };
+    veiculoServico.Incluir(veiculo);
+
+    return Results.Created($"/veiculo/{veiculo.Id}", veiculo);
+});
+#endregion
+#region APP
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.Run();
+#endregion
